967bbf8515356a6769318b6011ca1f4c2a4f3b6d
Tyson Gern <tyson@initialcapacity.io>
2024-04-20 09:51:59 -0600

    Improve analyzer logging

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -13,19 +13,19 @@ export default {
                 console.log(`skipping ${id}: ${link}`)
                 return
             }
-            console.log(`analyzing article ${id}: ${link}`)
 
+            console.log(`analyzing article ${id}: ${link}`)
             const article = await env.ARTICLES.get(link);
             console.log(`found content ${article?.slice(0, 20)}`)
+
             const embeddingResponse = await env.AI.run(
                 "@cf/baai/bge-large-en-v1.5",
                 {
                     text: [article],
                 }
             );
-
-            console.log(`found an embedding of length ${embeddingResponse.data.length}`)
             console.log(`found ${embeddingResponse.data.length} embeddings`)
+            console.log(`found an embedding of length ${embeddingResponse.data[0].length}`)
 
             await env.VECTORIZE_INDEX.upsert([{id, values: embeddingResponse.data[0], metadata: {link}}]);
             console.log(`embedding stored id: ${id}`)
