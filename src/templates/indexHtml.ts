10b10efa85684d260ee08215d24effac0ff19f94
Tyson Gern <tyson@initialcapacity.io>
2024-04-20 14:20:51 -0600

    Factor out heading and label

--- src/templates/indexHtml.ts
+++ src/templates/indexHtml.ts
@@ -6,42 +6,45 @@ type IndexProperties = {
     source?: string | undefined,
 };
 
-export const indexHtml = ({query, response, source}: IndexProperties = {}) => html`
-    <section>
-        <h1>What would you like to know?</h1>
-        <form action="/" method="post">
-            <fieldset>
-                <label>
-                    ${query === undefined
-                            ? html`Query`
-                            : html`New Query`}
-                    <input type="text" name="query">
-                </label>
-                <button type="submit">Ask</button>
-            </fieldset>
-        </form>
-    </section>
+export const indexHtml = ({query, response, source}: IndexProperties = {}) => {
+    const heading = query === undefined ? "What would you like to know?" : "What else would you like to know?"
+    const label = query === undefined ? "Query" : "New Query"
 
-    ${query === undefined
-            ? html``
-            : html`
-                <section>
-                    <h2>Question</h2>
-                    <p>${query}</p>
-                </section>
-            `}
-    ${response === undefined
-            ? html``
-            : html`
-                <section>
-                    <h2>Answer</h2>
-                    <p>${response}</p>
-                    ${source === undefined
-                            ? html``
-                            : html`
-                                <h3>Source</h3>
-                                <p><a href="${source}">${source}</a></p>
-                            `}
-                </section>
-            `}
-`
+    return html`
+        <section>
+            <h1>${heading}</h1>
+            <form action="/" method="post">
+                <fieldset>
+                    <label>
+                        ${label}
+                        <input type="text" name="query">
+                    </label>
+                    <button type="submit">Ask</button>
+                </fieldset>
+            </form>
+        </section>
+
+        ${query === undefined
+                ? null
+                : html`
+                    <section>
+                        <h2>Question</h2>
+                        <p>${query}</p>
+                    </section>
+                `}
+        ${response === undefined
+                ? null
+                : html`
+                    <section>
+                        <h2>Answer</h2>
+                        <p>${response}</p>
+                        ${source === undefined
+                        ? null
+                        : html`
+                                    <h3>Source</h3>
+                                    <p><a href="${source}">${source}</a></p>
+                                `}
+                    </section>
+                `}
+    `;
+}
